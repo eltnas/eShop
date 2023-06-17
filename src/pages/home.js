e638b2e502a3c10fe111209b2f0b1a0abab428e4
Elton Carlos do Nascimento <eltnas@outlook.com>
2023-06-17 18:54:44 -0300

    Add files via upload

---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+import * as Styles from './styles/home';
+
+const Home = ()=> {
+    return (
+            <Styles.Container>
+                <Styles.Tit>eShop</Styles.Tit>
+            </Styles.Container>
+    );
+}
+
+export default Home;
---
+++ src/pages/styles/home.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    height: 50px;
+    width: 120vw;
+    background-color: #66A9F8;
+    padding: 10px;
+`
+
+export const BoxTit = styled.div`
+    height: 100%;
+    width: 
+`
+
+export const Tit = styled.h1`
+    color: yellow;
+    font-family: 'Josefin Sans', sans-serif;
+    font-size: 2.4rem;
+    margin: 0 30px;
+`
+
+
+
